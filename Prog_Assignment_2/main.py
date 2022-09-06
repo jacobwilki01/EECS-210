56962b9d863c2c68073a0a9ea9d2d9f086e485e2
jacobwilki01 <jwilkus1@gmail.com>
2022-09-06 15:21:22 -0500

    Add Pre- and Post- Conditions. Start Comments

--- Prog_Assignment_2/main.py
+++ Prog_Assignment_2/main.py
@@ -1,28 +1,87 @@
-#Programming Assignment 2
+'''
+Code Name: EECS 210 - Programming Assignment #2
+Description: Runs the necessary code for proving or disproving each of predicates or quantifiers posed. 
 
+Programmer: Jacob Wilkus
+Date Created: September 1st, 2022
+
+Preconditions and Postconditions:
+    - main()
+        - Takes in nothing. 
+        - Runs at the start of the code. 
+        - Calls each of the relevant question functions.
+        - Returns nothing.
+    - equation functions:
+        - universal(list)
+            - takes in a list of truth values
+            - returns a boolean value of the value of the universal quantifier for the given list.
+        - existential(list)
+            - takes in a list of truth values
+            - returns a boolean value of the value of the existential quantifier for the given list.
+        - disjunction(list1,list2)
+            - takes in 2 lists of truth values and compares them against each other
+            - returns a singular list of the comparison results.
+        - negation(input)
+            - Takes in either a boolean or list of truth values
+            - Returns the opposite boolean or an inverse list.
+        - compare(val1,val2)
+            - compares two integers using less than.
+            - returns a boolean depending on the result
+        - if_zero(val1,val2)
+            - takes in two integers, and checks if one of them is zero by multiplying them together
+            - returns a boolean depending on the result of the multiplication
+    - proof functions:
+        - proof(mode,function,values)
+            - takes in a 'mode' string, 'function' string, and a 'values' list.
+            - the 'mode' determines if we are proving the existential quantifier or universal quantifier.
+            - the 'function' string is the words used to describe what the function is
+            - the 'values' list is iterated over to determine if the quantifiers hold true
+            - returns a string that includes proves the given function is either True or False.
+        - proof_large(mode,comp,func1,func2,values)
+            - same as proof(), except it takes in two functions ('func1' and 'func2') and a function comparison ('comp').
+            - returns a string that proves the given comparison beween the two functions is either True or False.
+        - proof_nested(mode_x,mode_y,values)
+            - same as proof() except it has two 'modes' ('mode_x' and 'mode_y') based on the two modes for nested quantifiers.
+            - returns a string that proves the nested quantifier either True or False based on the given modes.
+        - proof_demo(mode1,mode2,function,values)
+            - same as proof(), except for proving De Morgan's Law.
+            - has two modes, but compared to proof_nested, the 'mode1' refers to whether we are dealing with the negation or equivalent, and mode2 is if the given quantifier is existential or universal.
+            - returns a string that proves De Morgan's Law True for the relevant negation and equivalent quantifier.
+    - question functions: one_a(), one_b(), one_c(), one_d(), one_e(), one_f(), two_a(), two_b(), two_c(), two_d()
+        - takes in nothing
+        - called by main()
+        - returns a string that consists of a boolean for whether the certain equation is True or False, and the relevant proof function (as described above).
+'''
+
+#The main() function for the program. Simply calls each of the relevant question functions.
 def main():
+    #Creates a list of all of the functions listed under #1 on the assignment sheet. Iterates over that list, calling them all, and prints their result.
     one = [one_a(),one_b(),one_c(),one_d(),one_e(),one_f()]
     for letter in one:
         print(letter)
 
-    print("")
+    print("") #Prints a simple blank line to separate the answers to #1 and #2.
 
+    #Creates a list of all of the functions listed under #2 on the assignment sheet. Iterates over that list, calling them all, and prints their result.
     two = [two_a(),two_b(),two_c(),two_d()]
     for letter in two:
         print(letter)
 
+#Function for universal. Just iterataes over a list of truth values and determines if they are all true. Returns a boolean based on that iteration.
 def universal(list):
     for value in list:
         if value == False:
             return False
     return True
 
+#Function for existential. Just iterataes over a list of truth values and determines if they are all true. Returns a boolean based on that iteration.
 def existential(list):
     for value in list:
         if value == True:
             return True
     return False
 
+#Function for disjunction. Just iterataes over two lists of truth values and determines if either one or the other is true. Returns a list of truth values based on that iteration.
 def disjunction(list1,list2):
     result = []
     for x in range(0,len(list1)):
@@ -44,7 +103,6 @@ def negation(input):
             else:
                 result.append(True)
         return result
-
 
 def compare(val1,val2):
     if val1 < val2:

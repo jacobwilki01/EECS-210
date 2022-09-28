e83d4a07b1a813f1d53186f253fde85939054fe2
jacobwilki01 <jwilkus1@gmail.com>
2022-09-28 13:09:19 -0500

    Update main.py

--- Prog_Assignment_3/main.py
+++ Prog_Assignment_3/main.py
@@ -12,91 +12,122 @@ Preconditions and Postconditions:
         -Prints their outputs.
     -operation functions
         -union(rel1,rel2)
+            -Creates the union of two relations
             -Takes in two relations in the form of lists
             -Appends all of their entries into a new list, then turns that list into a set to remove redundant values.
             -Then returns the set as a list using python's built-in sorted() function
         -intersect(rel1,rel2)
+            -Creates the intersection of two relations
             -Takes in two relations in the form of lists
             -Iterates over the first relation, then for each iteration, iterates over the second.
             -Appends values to a new list for each iteration if the value in the first relation is equal to that in the second.
             -Returns that list.
         -diff(rel1,rel2)
+            -Creates the difference between two relations
             -Takes in two relations in the form of lists
             -Iterates over the first relation, and appends them all to a new list.
             -Then does nested iteration over both relations, and removes values from the new list if they exist in both relation 1 and 2.
             -Returns that list.
         -compose(rel1,rel2)
+            -Creates the composition of two relations
             -Takes in two relatons in the form of lists
             -Does nested iteration over both, and if the 'y' value of a given ordered pair in the first relation is the same as an 'x' value in a given ordered pair of the second, 
              it then appends to a new list an ordered pair containing the 'x' of the first relation's pair and the 'y' value of the second relation's pair.
             -returns that list
         -ordered_pair(set)
+            -Creates the set of ordered pairs to use for #4.
             -Takes in a set in the form of a list.
             -Iterates over each value in it, and then iterates a second time. If the two relevant values are the same, it appends them in the form of an ordered pair to a new list.
             -Returns that new list.
         -reflex(set, relation)
+            -Checks if a relation is reflexive
+            -Takes in a set and a relation in the form of lists
+            -Iterates over the given set and relation to see if any number in the set makes the relation not reflexive
+            -Returns a string describing whether or not it is reflexive
         -symmetric(relation)
+            -Checks if a releation is symmetric
+            -Takes in a relation in the form of a list
+            -Iterates over the given relation twice, and checks if the relation is reflexive
+            -Returns a list signifying the result, and if it is False, the ordered pair that makes it false.
         -antisymmetric(relation)
+            -Checks if a relation is antisymmetric
+            -Follows the same code as symmetric(), except it returns the opposite results.
         -transitive(relation)
+            -Checks if a relation is transitive
+            -Takes in a relation in the form of a list
+            -Does three levels of nested iteration to check that all three ordered pairs exist
+            -Returns a string with information for if it is or is not transitive. If it is not, the string includes the relevant ordered pairs.
     -question functions: one_a(r1,r2), one_b(r1,r2), one_cr1,r2), one_d(r1,r2), two(r,s), three(r), four_a(set), four_b(set), four_c(set), four_d(set)
+        -Takes in either a set, relation, or both. It may also take in two of one. All in the form of lists.
+        -Calls on one of the operation functions above to determine the meat of the question.
+        -Returns a string with the relevant results.
 '''
 
+#The main function for the program.
 def main():
-    r1, r2 = [(1,1),(2,2),(3,3)], [(1,1),(1,2),(1,3),(1,4)]
-    one = [one_a,one_b,one_c,one_d]
+    #The following code answers the questions for #1.
+    r1, r2 = [(1,1),(2,2),(3,3)], [(1,1),(1,2),(1,3),(1,4)] #Creates each of the relations as given in the assignment worksheet.
+    one = [one_a,one_b,one_c,one_d] #Holds a list of each of the functions, that is then iterated over and each function is then called.
     for i in one:
-        print(i(r1,r2))
+        print(i(r1,r2)) #Prints the result of each question.
 
-    r, s = [(1,1),(1,4),(2,3),(3,1),(3,4)], [(1,0),(2,0),(3,1),(3,2),(4,1)]
-    print(two(r,s))
-    print(three(r))
+    #The following code answers questions 2 and 3.
+    r, s = [(1,1),(1,4),(2,3),(3,1),(3,4)], [(1,0),(2,0),(3,1),(3,2),(4,1)] #Creates the relations R and S used for both questions.
+    print(two(r,s)) #Calls on, and prints the result of two.
+    print(three(r)) #Calls on, and prints the result of three.
 
+    #The following code creates the set used in #4 by iterating from -10 to 10 and appending it to a list.
     four_set = []
     for x in range(-10,11):
         four_set.append(x)
     
-    four = [four_a,four_b,four_c,four_d,four_e]
+    #The following code runs each of the relevant parts of #4.
+    four = [four_a,four_b,four_c,four_d,four_e] #Holds a list of each of the functions, that is then iterated over and each function is then called.
     for i in four:
-        print(i(four_set))
+        print(i(four_set)) #Prints the result of each question.
 
+#The following code is for the union() function.
 def union(rel1,rel2):
-    result = []
-    for x in rel1:
-        result.append(x)
-        for y in rel2:
-            result.append(y)
+    result = [] #Creates a blank list to hold the results of the function.
+    for x in rel1: #iterates over the values in the first relation
+        result.append(x) #appends those values to the new list.
+        for y in rel2: #iterates over the values in the second relation
+            result.append(y) #appends those values to the new list
     
-    return sorted(set(result))
+    return sorted(set(result)) #returns the list sorted and with redundant items removed.
 
+#Code for the intersect() function.
 def intersect(rel1,rel2):
-    result = []
-    for x in rel1:
-        for y in rel2:
-            if x == y:
+    result = [] #creates a blank list to hold the results of the function
+    for x in rel1: #iterates over the items in the first relation
+        for y in rel2: #iterates over the items in the second relation
+            if x == y: #checks if the values are equal, then appends the value to the new list
                 result.append(x)
     
-    return result
+    return result #returns that new list
 
+#Code for the diff() function.
 def diff(rel1,rel2):
-    result = []
-    for i in rel1:
+    result = [] #creates a blank list to hold the result of the code
+    for i in rel1: #iterates over the first relation, and appends each item to the new list
         result.append(i)
 
-    for x in rel1:
-        for y in rel2:
-            if x == y:
+    for x in rel1: #iterates over the first relation
+        for y in rel2: #iterates over the second relation
+            if x == y: #if any of the values equal each other, it is removed from the list
                 result.remove(x)
     
-    return result
+    return result #returns the list
 
+#Code for the compose() function.
 def compose(rel1,rel2):
-    result = []
-    for x in rel1:
-        for y in rel2:
-            if x[1] == y[0]:
-                result.append((x[0],y[1]))
+    result = [] #Creates a blank list to hold the results
+    for x in rel1: #iterates over the first relation
+        for y in rel2: #iterates over the second relation
+            if x[1] == y[0]: #if the 'y' value of the first pair, and the 'x' value of the second are the same, it runs the following
+                result.append((x[0],y[1])) #appends an ordered pair of the 'x' value of the first pair and 'y' value of the second to the new list
 
-    return result
+    return result #returns the list
 
 def ordered_pair(set):
     result = []

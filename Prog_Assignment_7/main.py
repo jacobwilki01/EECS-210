91c70fc09e20a316f0fbea998e63c6751b2e73ee
jacobwilki01 <jwilkus1@gmail.com>
2022-11-21 17:41:34 -0600

    Update main.py

--- Prog_Assignment_7/main.py
+++ Prog_Assignment_7/main.py
@@ -25,6 +25,23 @@ def sterling2(n,k):
 def combination(n,r):
     return factorial(n) / (factorial(r) * factorial(n-r))
 
+def dist_dist(objects,boxes,per_box):
+    box = []
+    for _ in range(boxes):
+        box.append(per_box)
+
+    temp = 0
+    denom = 1
+    for i in range(len(box)):
+        temp += box[i]
+        box[i] = factorial(box[i])
+        denom *= box[i]
+
+    if temp != objects:
+        denom *= factorial(objects-temp)
+
+    return factorial(objects) / denom
+
 def indist_dist(objects,boxes):
     return combination(objects+boxes-1,boxes-1)
 
@@ -35,7 +52,7 @@ def indist_indist(objects,boxes):
     return partition(objects,boxes)
 
 def main():
-    print("1.)")
+    print("1.) There are",dist_dist(52,4,5),"ways to distribute 40 objects into 4 boxes with 10 objects in each box.")
     print("2.) There are", indist_dist(12,6), "ways to distribute 12 indistinguishable objects into 6 distinguishable boxes")
     print("3.) There are", dist_indist(5,4), "ways to distribute 5 distinguishable objects into 4 indistinguishable boxes")
     print("4.) There are", indist_indist(5,3),"ways to distribute 5 indistinguishable objects into 3 indistinguishable boxes")
